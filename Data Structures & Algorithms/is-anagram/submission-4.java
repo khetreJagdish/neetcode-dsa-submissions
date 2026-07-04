79ca5214ca89e74f91ca784714164834bb008288
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2026-07-04 15:56:57 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        s = s.replace(" ","");
+        t = t.replace(" ","");
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] count = new int[26];
+        
+        for(int i = 0; i<s.length();i++){
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i<t.length();i++){
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int num : count){
+            if(num != 0){
+                return false;
+            }
+        }
+
+    return true;
+    }
+}
