c2e3aabfc08d6ebc8f0eb452bf2b169201a7a241
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2026-07-19 15:32:04 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // use Greedy Approah
+        int maxProfit = 0;
+        int n = prices.length;
+        for(int i = 1; i<n; i++){
+            if(prices[i-1] < prices[i]){
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+    return maxProfit;
+    }
+}
