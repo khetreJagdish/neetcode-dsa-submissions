a1b1d80a9145b092800b3c070162387ffe580db1
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2026-07-31 17:21:26 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int totalArea = 0;
+        // find prefixMax 
+        int[] prefixMax = new int[n];
+        prefixMax[0] = height[0];
+        for(int i = 1; i<n; i++){
+            prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
+        }
+
+        //find suffixMax 
+        int[] suffixMax = new int[n];
+        suffixMax[n-1] = height[n-1];
+        for(int i = n-2; i>= 0; i--){
+            suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
+        }
+
+        // then just check whether the current bar is less than it's leftMax and it's rightMax 
+        //(because then only water can be stored between)
+        for(int i = 0; i<n; i++){
+
+            if(prefixMax[i] > height[i] && suffixMax[i] > height[i]){
+                totalArea += Math.min(prefixMax[i],suffixMax[i]) - height[i];
+            }
+        }
+    return totalArea;
+    }
+}
