735bf29982b8a625ca344dff806d722e6ccf61ee
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2026-08-07 20:14:41 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+        for(int i = 0; i<prices.length; i++){
+
+            for(int j = i; j<prices.length; j++){
+
+                maxProfit = Math.max(maxProfit,prices[j] - prices[i]);
+            }
+        }
+    return maxProfit;
+    }
+}
