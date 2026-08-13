f63eaa7ee1d65dd36a6efa650a676a5eea57aae6
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2026-08-13 11:32:42 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        int[] freq = new int[256];
+        while(right < s.length()) {
+
+            while(freq[s.charAt(right)] == 1){
+                freq[s.charAt(left)] = 0;
+                left++;
+            }
+            freq[s.charAt(right)] = 1;
+            maxLength = Math.max(maxLength, right-left+1);
+            right++;
+        }
+    return maxLength;
+    }
+}
